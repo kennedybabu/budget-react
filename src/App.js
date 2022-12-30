bf4b5549c10ddb6e7f01099d5b6196e3d43c0b0d
kennedybabu <kennedynjogu8@gmail.com>
2022-12-30 10:29:10 +0300

    feature: delete an entry

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import {  Container, Grid, Icon, Segment } from "semantic-ui-react";
+import { useState } from "react";
+import {  Container } from "semantic-ui-react";
 import "./App.css"
 import DisplayBalance from "./components/DisplayBalance";
 import DisplayBalances from "./components/DisplayBalances";
-import EntryLine from "./components/EntryLine";
+import EntryLines from "./components/EntryLines";
 import MainHeader from "./components/MainHeader";
 import NewEntryForm from "./components/NewEntryForm";
 
 function App() {
+  const [entries, setEntries]= useState(initialEntries)
+
+  const deleteEntry = (id) => {
+    const result = entries.filter(entry => entry.id !== id)
+    setEntries(result)
+  }
   return (
     <Container>
       <MainHeader title="Budget"/>
@@ -15,10 +22,9 @@ function App() {
 
       <DisplayBalances />
 
-      <MainHeader type="h3" title="History" />    
+      <MainHeader type="h3" title="History" />
 
-      <EntryLine  description="Income" value='$10.00'/>
-      <EntryLine  description="Expense" value='$20.00' isExpense/>
+      <EntryLines entries={entries} deleteEntry={deleteEntry}/>
 
       <MainHeader type="h3" title='Add new transaction' />
       <NewEntryForm />
@@ -27,3 +33,31 @@ function App() {
 }
 
 export default App;
+
+
+var initialEntries = [
+  {
+    id:1,
+    description:"Work income",
+    value: "$1000.00",
+    isExpense:false
+  },
+  {
+    id:2,
+    description:"Water Bill",
+    value: "$20.00",
+    isExpense:true
+  },
+  {
+    id:3,
+    description:"Rent",
+    value: "$300.00",
+    isExpense:true
+  },
+  {
+    id:4,
+    description:"Power bill",
+    value: "$50.00",
+    isExpense:true
+  }
+]
--- src/components/EntryLine.jsx
+++ src/components/EntryLine.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { Segment, Grid, Icon } from 'semantic-ui-react'
 
-function EntryLine({description, value, isExpense=false}) {
+function EntryLine({id, description, value, isExpense=false, deleteEntry}) {
   return (
     <Segment color={isExpense ? 'red' :'green'}>
-    <Grid columns={3} textAlign='right'>
-      <Grid.Row>
-        <Grid.Column width={10} textAlign='left'>{description}</Grid.Column>
-        <Grid.Column width={3} textAlign='right'>{value}</Grid.Column>
-        <Grid.Column width={3}>
-          <Icon name="edit" />
-          <Icon name="trash" />
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
-  </Segment>
+      <Grid columns={3} textAlign='right'>
+        <Grid.Row>
+          <Grid.Column width={10} textAlign='left'>{description}</Grid.Column>
+          <Grid.Column width={3} textAlign='right'>{value}</Grid.Column>
+          <Grid.Column width={3}>
+            <Icon name="edit" />
+            <Icon name="trash" onClick={() =>deleteEntry(id)}/>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
   )
 }
 
