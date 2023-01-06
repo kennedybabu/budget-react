b1786cd12232f59079365400c81eb8a86c9085de
kennedybabu <kennedynjogu8@gmail.com>
2023-01-06 10:15:18 +0300

    feature: add redux-saga to the project

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,15 +1,22 @@
-import { combineReducers, createStore } from "redux"
+import { applyMiddleware, combineReducers, createStore } from "redux"
 import entriesReducers from "../reducers/entries.reducers"
 import {composeWithDevTools} from "redux-devtools-extension"
 import modalsReducer from "../reducers/modals.reducers"
+import createSagaMiddleware from "@redux-saga/core"
+import { testSaga } from "../sagas/testSaga"
+
+const sagaMiddleware = createSagaMiddleware()
+const middlewares = [sagaMiddleware]
 
 const configureStore = () => {
-    return createStore(combineReducers({
+    const store = createStore(combineReducers({
         entries: entriesReducers,
         modals: modalsReducer
     }),
-    composeWithDevTools()
+    composeWithDevTools(applyMiddleware(...middlewares))
     )
+    sagaMiddleware.run(testSaga)
+    return store
 }
 
 export default configureStore
