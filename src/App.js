1095bfc708e014c140ba95f97d9556a46bb72522
kennedybabu <kennedynjogu8@gmail.com>
2023-01-08 09:00:41 +0300

    feature: fetch data using axios in saga

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
 
   useEffect(() => {
     dispatch(getAllEntries())
-  }, [])
+  }, [dispatch])
 
   return (
     <Container>
--- src/sagas/entriesSaga.js
+++ src/sagas/entriesSaga.js
@@ -1,10 +1,12 @@
+import axios from "axios";
 import { call, put, take } from "redux-saga/effects";
+import types from "../actions/entries.actions";
 import entriesTypes from '../actions/entries.actions'
 
 export function* getAllEntries(){
     yield take(entriesTypes.GET_ENTRIES)
     console.log('get entries now')
-    const results = yield call(axios, 'https://localhost:3002/entries')
+    const results = yield call(axios, 'http://localhost:3002/entries')
     console.log(results)
-    yield put({type: entriesTypes.POPULATE_ENTRIES, payload: results.data})
+    yield put({type: types.POPULATE_ENTRIES, payload: results.data})
 }
