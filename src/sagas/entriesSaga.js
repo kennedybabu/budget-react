28526dcf1c5a9e912a8a25abd75e022b2fc98b11
kennedybabu <kennedynjogu8@gmail.com>
2023-01-10 07:46:49 +0300

    feature: refactor the entriesSaga gen functions

--- src/sagas/entriesSaga.js
+++ src/sagas/entriesSaga.js
@@ -1,22 +1,20 @@
 import axios from "axios";
 import { call, fork, put, take } from "redux-saga/effects";
-import types from "../actions/entries.actions";
+import { populateEntries, populateEntryDetails } from "../actions/entries.actions";
 import entriesTypes from '../actions/entries.actions'
 
 export function* getAllEntries(){
     yield take(entriesTypes.GET_ENTRIES)
     console.log('get entries now')
-    const results = yield call(axios, 'http://localhost:3002/entries')
-    console.log(results)
-    yield put({type: types.POPULATE_ENTRIES, payload: results.data})
+    const {data} = yield call(axios, 'http://localhost:3002/entries')
+    yield put(populateEntries(data))
 }
 
 export function* getEntryDetails(id){
     // console.log(`got this ${id}`)
     const { data } = yield call(axios, `http://localhost:3002/values/${id}`)
-    yield put({type:entriesTypes.POPULATE_ENTRIES_DETAILS, payload:{entry:data, id}})
+    yield put(populateEntryDetails(id, data))
 }
-
 
 export function* getAllEntriesDetails(){
     const {payload} = yield take(entriesTypes.POPULATE_ENTRIES)
